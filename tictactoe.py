e46d37185539624ded18527deac370433d565e98
lewisssmith19 <97068557+lewisssmith19@users.noreply.github.com>
2022-01-10 15:51:14 -0700

    Add files via upload
    W02 Prove tictactoe

---
+++ tictactoe.py
@@ -0,0 +1,56 @@
+#tictactoe.py
+#Lewis Smith
+
+def main():
+    player = next_player("")
+    board = create_board()
+    while not (winner(board) or draw(board)):
+        display_board(board)
+        move(player, board)
+        player = next_player(player)
+    display_board(board)
+    print("Well done! Thank you for playing!") 
+
+def create_board():
+    board = []
+    for square in range(9):
+        board.append(square + 1)
+    return board
+
+def display_board(board):
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
+    
+def draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True 
+    
+def winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
+
+def move(player, board):
+    square = int(input(f"Player {player}'s turn to choose a square (1-9): "))
+    board[square - 1] = player
+
+def next_player(current):
+    if current == "" or current == "o":
+        return "x"
+    elif current == "x":
+        return "o"
+
+if __name__ == "__main__":
+    main()
